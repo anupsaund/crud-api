d38bf62ad23812fb5c3c35183b6a8e6b22b22291
Anup Saund <anup.saund@gmail.com>
2019-07-28 13:21:53 -0700

    update

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-var http = require('http');
+var http            = require('http')
+var HttpDispatcher  = require('httpdispatcher')
+var dispatcher      = new HttpDispatcher()
+
 var server = http.createServer(function(req, res) {
+    dispatcher.dispatch(req, res)
+});
+
+
+dispatcher.onGet("/", function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var message = 'It works!\n',
         version = 'NodeJS ' + process.versions.node + '\n',
         response = [message, version].join('\n');
     res.end(response);
-});
-server.listen();
+}); 
+
+server.listen()
