cf69b79db594c6e832779f4431ef8cf159dcd04c
Anup Saund <anup.saund@gmail.com>
2019-07-28 14:55:57 -0700

    testing express

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
-var http            = require('http')
-var HttpDispatcher  = require('httpdispatcher')
-var dispatcher      = new HttpDispatcher()
+var createError     = require('http-errors')
+var express         = require('express')
+var path            = require('path')
+var cookieParser    = require('cookie-parser')
+var logger          = require('morgan')
+var env             = require('dotenv').config()
+var indexRouter     = require('./routes/index')
+var usersRouter     = require('./routes/users')
 
-var server = http.createServer(function(req, res) {
-    dispatcher.dispatch(req, res)
+var app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
 });
 
-
-dispatcher.onGet("/", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-}); 
-
-dispatcher.onGet("/test", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    
-    res.end("Testing");
-}); 
-
-server.listen()
+module.exports = app
