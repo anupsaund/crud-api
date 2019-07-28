16818b431bf2603f5e71d70e8316e16f7d0b5156
Anup Saund <anup.saund@gmail.com>
2019-07-28 12:27:41 -0700

    Setting up express

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/* var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var message = 'It works!\n',
@@ -7,3 +7,12 @@ var server = http.createServer(function(req, res) {
     res.end(response);
 });
 server.listen();
+*/
+
+const express = require('express')
+const app = express()
+const port = 80
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
