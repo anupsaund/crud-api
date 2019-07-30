131699908aed8de22cf40451b5abb5d8e7f56775
Anup Saund <anup.saund@gmail.com>
2019-07-29 20:18:42 -0700

    cors@

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError     = require('http-errors')
+var cors = require('cors')
 var express         = require('express')
 var path            = require('path')
 var cookieParser    = require('cookie-parser')
@@ -32,6 +33,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
 app.use(logger('dev'))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
